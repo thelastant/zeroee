f71dd6b06e482211f825463fd76c88cef463d6ce
myname <myemail@email.com>
2018-08-28 23:50:52 +0800

    ww

--- zoroeee/settings.py
+++ zoroeee/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'zoroeee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': "45.63.51.252",
+        'PORT': 3306,
+        'NAME': "zoro",
+        'USER': "root",
+        'PASSWORD': "123456"
+    },
 }
 
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
